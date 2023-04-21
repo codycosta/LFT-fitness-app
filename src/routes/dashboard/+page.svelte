2deee2b42be6e06c0aa6c4e48c362042694ea6ab
Cody <costa.cody7@gmail.com>
2023-04-20 20:53:01 -0700

    added some basic content to dashboard

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import UserIntroduction from "./UserIntroduction.svelte";
+
+    import { state } from "../../stores/dark-mode-state"
+
+    let mainBackgroundColor;
+    let mainTextColor;
+
+    $: {
+        mainBackgroundColor = $state ? '#292B2F' : 'white';
+        mainTextColor = $state ? 'lightgrey' : 'black';
+    }
+</script>
+
+<main
+    style:background-color={mainBackgroundColor}
+    style:color={mainTextColor}
+>
+    <UserIntroduction />
+    
+</main>
+
+<style>
+    main {
+        min-height: 100vh;
+        transition: 500ms;
+    }
+</style>
---
+++ src/routes/dashboard/UserIntroduction.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<div class="user-intro">
+    <p>Hi, username!</p>
+</div>
+
+<style>
+    .user-intro {
+        padding: 60px 20px;
+        font-family: 'Rubik';
+        font-size: 2rem;
+    }
+</style>
